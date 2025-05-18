d13a916a63c5e286f557040963a413c00ba2abfc
Nityanand <you@example.com>
2025-05-18 14:08:50 +0530

    removed extra code

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import Dashboard from "./pages/Dashboard";
 import Sidebar from "./pages/Sidebar";
 
 const App = () => {
   return (
     <div>
       <Sidebar />
-      {/* <Dashboard /> */}
     </div>
   );
 };
